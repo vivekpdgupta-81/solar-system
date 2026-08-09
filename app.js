59785de142c092645857eacc5075a92769cfa57a
Vivek <vivek.prasad.gupta@accenture.com>
2026-08-09 14:51:39 +0000

    fix: upgrade mongoose to v8, use env vars for Atlas connection

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const OS = require('os');
@@ -13,16 +14,10 @@ app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
-    if (err) {
-        console.log("error!! " + err)
-    } else {
-      //  console.log("MongoDB Connection Successful")
-    }
+    pass: process.env.MONGO_PASSWORD
 })
+.then(() => console.log("MongoDB Connection Successful"))
+.catch((err) => console.log("error!! " + err));
 
 var Schema = mongoose.Schema;
 
@@ -36,20 +31,21 @@ var dataSchema = new Schema({
 });
 var planetModel = mongoose.model('planets', dataSchema);
 
+app.post('/planet', async function(req, res) {
+    try {
+        const planetData = await planetModel.findOne({
+            id: parseInt(req.body.id)
+        });
 
-
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
-    planetModel.findOne({
-        id: req.body.id
-    }, function(err, planetData) {
-        if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
+        if (!planetData) {
+            res.send("Planet not found");
         } else {
             res.send(planetData);
         }
-    })
+    } catch (err) {
+        console.error(err);
+        res.send("Error in Planet Data");
+    }
 })
 
 app.get('/',   async (req, res) => {
